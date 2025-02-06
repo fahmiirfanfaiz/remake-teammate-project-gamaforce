690c457c85d388a340d08fc852220b1a31880b91
Fahmi Irfan Faiz <fahmiirfanfaiz01@gmail.com>
2025-02-07 00:24:05 +0700

    feat: website project setup

---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+
+const Navbar = () => {
+    return(
+        <div>
+            Hello World!
+        </div>
+    )
+};
+
+export default Navbar;
