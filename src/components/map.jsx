a1dc5e7afbdf60214f73fde558917aa72cccece0
diankartika <diankartika487@gmail.com>
2025-02-07 16:32:32 +0700

    fix(map): make the map not overlaying the navbar

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -7,18 +7,22 @@ const DefaultMap = () => {
     const [center, setCenter] = useState({lat: -7.769961708941515, lng: 110.37822795057816});
     const ZoomLevel = 18;
     const MapRef = useRef();
-    return(
-        <div className="row z-49">
-            <div className="col">
-                <MapContainer className="w-screen h-screen" center={center} zoom={ZoomLevel} ref={MapRef}>
-                    <TileLayer
-                    url="https://api.maptiler.com/tiles/satellite-v2/{z}/{x}/{y}.jpg?key=05qVMP6EtS9y6nQdaj4j"
-                    attribution={`&copy; <a href="https://www.maptiler.com/">MapTiler</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors`}
-                    />
-                </MapContainer>
-            </div>
+    return (
+        <div className="relative">
+          {/* Peta akan menyesuaikan dengan navbar */}
+          <MapContainer
+            className="absolute top-[6.5vw] left-0 w-screen h-[calc(100vh-6.5vw)] z-0"
+            center={center}
+            zoom={ZoomLevel}
+            ref={MapRef}
+          >
+            <TileLayer
+              url="https://api.maptiler.com/tiles/satellite-v2/{z}/{x}/{y}.jpg?key=05qVMP6EtS9y6nQdaj4j"
+              attribution={`&copy; <a href="https://www.maptiler.com/">MapTiler</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors`}
+            />
+          </MapContainer>
         </div>
-    )
-}
+    );
+};
 
 export default DefaultMap;
